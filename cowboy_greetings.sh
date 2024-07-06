124e5906071ebde806723a54b85bf00769556c33
Gabriel de Azeredo <gabrielfreitas.azeredo@gmail.com>
2024-07-06 16:45:33 -0300

    Add files and update readme

---
+++ cowboy_greetings.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+
+function cowboy_greetings
+
+    set quotes "SEE YOU SPACE COWBOY..." "SEE YOU SPACE COWGIRL, SOMEDAY, SOMEWHERE!" "ARE YOU LIVING IN THE REAL WORLD?" "EASY COME, EASY GO..." "YOU'RE GONNA CARRY THAT WEIGHT."
+    set_color brblue
+    echo (random choice $quotes)
+end
+
+cowboy_greetings
